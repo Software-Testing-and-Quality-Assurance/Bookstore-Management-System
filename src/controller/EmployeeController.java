82f63299a0f6e9955dcf8b013c8462fec8e7553e
Keit Nika <@keitnika>
2024-12-27 18:57:45 +0100

    EmployeeController.java fixes

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,42 +1,34 @@
 package controller;
 import main.Main;
-
-
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
 
-import controller.HeaderlessObjectOutputStream;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import model.Employee;
 
 public class EmployeeController {
 
 	public void loadUsersFromFile() {
 	    try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(Main.DATA_FILE))) {
-	        while (true) {
-	                Employee employee = (Employee) inputStream.readObject();
-	                Main.employeesAll.add(employee);
-	                System.out.println("add one");
-	        }
-	        
+
+			while (true) {
+				try {
+					Employee employee = (Employee) inputStream.readObject();
+					Main.employeesAll.add(employee);
+					System.out.println("add one");
+				} catch (EOFException e) {
+					break;
+				}
+			}
 	    }  catch (EOFException ignored) {
 	    	for (Employee e: Main.employeesAll) {
 	        	System.out.println(e.toString()+"check: "+e.getCheckBooks()+" "+e.getDateTerminated());
 	        }
 	        System.out.println("Data loaded from file! "+ Main.employeesAll.size());
-        }catch (FileNotFoundException e) {
-	        System.out.println(e.getMessage());
+        } catch (IOException ex) {
+	        System.out.println(ex.getMessage());
 	    } catch (ClassNotFoundException ex) {
 	        System.out.println("Class not found");
-	    } catch (IOException ex) {
-	        System.out.println(ex.getMessage());
 	    }
-	}
+    }
 	
 	
 	public boolean create(Employee employee) {
@@ -59,24 +51,6 @@ public class EmployeeController {
 	    }
 		return false;
 	}
-	
-	public boolean delete(Employee employee) {
-        try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(Main.DATA_FILE))) {
-            for(Employee e : Main.employeesAll) {
-                if(!e.equals(employee))
-                    outputStream.writeObject(e);
-                else {
-                	System.out.println("fired");
-                }
-            }
-            Main.employeesAll.remove(employee);
-            Main.doneAlert();
-            return true;
-        } catch (IOException ex) {
-            return false;
-        }
-    }
-	
 	public Employee searchEmployee(String username) {
 		for (Employee e : Main.employeesAll)
 			if(e.getUsername().equals(username))
@@ -87,11 +61,8 @@ public class EmployeeController {
 	public boolean employeeFound(String username) {
 		if(Main.employeesAll.isEmpty())
 			return false;
-		if(searchEmployee(username)==null)
-				return false;
-		return true;
-	
-	}
+        return searchEmployee(username) != null;
+    }
 	 
 	public boolean updateAll() {
 	        try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(Main.DATA_FILE))) {
