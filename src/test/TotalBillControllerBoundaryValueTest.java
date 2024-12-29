b3127f692cfb08bf4af1c06b688cf39825f54930
klaudia tamburi <ktamburi22@epoka.edu.al>
2024-12-29 01:49:02 +0100

    license comment in testing files for mockito and junit

--- src/test/TotalBillControllerBoundaryValueTest.java
+++ src/test/TotalBillControllerBoundaryValueTest.java
@@ -8,6 +8,18 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
+/*
+ * JUnit 5 License:
+ * This code uses JUnit 5, licensed under the Eclipse Public License 2.0.
+ * See: http://www.eclipse.org/legal/epl-2.0/
+ */
+
+/*
+ * Mockito License:
+ * This code uses Mockito, licensed under the MIT License.
+ * See: https://opensource.org/licenses/MIT
+ */
+
 package test;
 
 import controller.TotalBillController;
