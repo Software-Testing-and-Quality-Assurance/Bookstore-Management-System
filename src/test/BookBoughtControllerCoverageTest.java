86b7f88a746e45fcaca2d3b78a3d6310d6509e33
klaudia tamburi <ktamburi22@epoka.edu.al>
2024-12-30 07:00:37 +0100

    small fixes in testing code x2 :p - ps. i should sleep

--- src/test/BookBoughtControllerCoverageTest.java
+++ src/test/BookBoughtControllerCoverageTest.java
@@ -4,7 +4,7 @@
  * * * * * * * * * Project: Code Testing * * * * * * *
  * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * * * * * * * * * Coverage  Testing * * * * * * * * *
- * * * * * Test done by: Klaudia Tamburi * * * * * * *
+ * * * * * * Test done by: Klaudia Tamburi * * * * * *
  * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
@@ -14,7 +14,6 @@ import controller.BookBoughtController;
 import javafx.collections.ObservableList;
 import model.Author;
 import model.Book;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,8 +30,9 @@ class BookBoughtControllerCoverageTest {
     * Test Covarage Test Cases
     * Statament Coverage: Execution of each code line at least once (1 test case - 95% coverage)
     * Branch Coverage: Execution of each code branch at least once (1 test case - 95% coverage)
-    * Condition Coverage: Execution of each possible condition combination at least once (4 test cases - 100% coverage)
-    * MC/DC: 100% code execution with the least amount of input tests taking into consideration all conditions and branches (4 test cases - 100% coverage)
+    * Condition Coverage: Execution of each possible condition combination at least once (4 dates test cases - 100% coverage)
+    * MC/DC: 100% code execution with the least amount of input tests taking into consideration all conditions and branches
+    * (4 dates test cases or 3 books in stock 2 dates test cases - 100% coverage)
      */
 
     private BookBoughtController controller;
@@ -96,7 +96,7 @@ class BookBoughtControllerCoverageTest {
         //We care for each code branch to be executed; case covers when if condition is entered and not entered; again we don't achieve 100% coverage
         Calendar cal = Calendar.getInstance();
 
-        /// Condition test: Both conditions inside the if statement evaluate T
+        // Condition test: Both conditions inside the if statement evaluate T
         cal.set(2018, Calendar.DECEMBER, 31);
         Date date1 = cal.getTime();
         cal.set(2019, Calendar.OCTOBER, 3);
@@ -106,7 +106,7 @@ class BookBoughtControllerCoverageTest {
         assertEquals(1,filteredBooks.size());
         assertTrue(filteredBooks.stream().anyMatch(book -> book.getTitle().equals("Title1")));
 
-        /// Condition test: Both conditions inside the if statement evaluate F
+        // Condition test: Both conditions inside the if statement evaluate F
         cal.set(2019, Calendar.OCTOBER, 3);
         date1 = cal.getTime();
         cal.set(2019, Calendar.AUGUST, 2);
@@ -115,7 +115,7 @@ class BookBoughtControllerCoverageTest {
         filteredBooks = controller.filterDate(date1, date2, bookStock);
         assertTrue(filteredBooks.isEmpty());
 
-        /// Condition test: First condition evaluates T and second F
+        // Condition test: First condition evaluates T and second F
         cal.set(2019, Calendar.SEPTEMBER, 1);
         date1 = cal.getTime();
         cal.set(2019, Calendar.SEPTEMBER, 2);
@@ -124,7 +124,7 @@ class BookBoughtControllerCoverageTest {
         filteredBooks = controller.filterDate(date1, date2, bookStock);
         assertTrue(filteredBooks.isEmpty());
 
-        /// Condition test: First condition evaluates F and second T
+        // Condition test: First condition evaluates F and second T
         cal.set(2019, Calendar.NOVEMBER, 1);
         date1 = cal.getTime();
         cal.set(2019, Calendar.NOVEMBER, 2);
@@ -143,7 +143,7 @@ class BookBoughtControllerCoverageTest {
         // for the condition to evaluate F&&F, d1>d2
         // for 100% code
 
-        /// Set up the other books needed in stock
+        // Set up the other books needed in stock
         Calendar cal = Calendar.getInstance();
         cal.set(2019, Calendar.JANUARY, 2);
         Date date = cal.getTime();
@@ -152,7 +152,7 @@ class BookBoughtControllerCoverageTest {
         date = cal.getTime();
         bookStock.add(new Book("ISBN3", "Title3", "Supplier3", "Category3", 70.0, 50.0, new Author("Author3", "3"), 10, date));
 
-        /// The first three cases d1<d2, T&&T, F&&T, T&&F
+        // The first three cases d1<d2, T&&T, F&&T, T&&F
         cal.set(2019, Calendar.OCTOBER, 1);
         Date date1 = cal.getTime();
         cal.set(2019, Calendar.OCTOBER, 3); 
@@ -161,7 +161,7 @@ class BookBoughtControllerCoverageTest {
         assertEquals(1, filteredBooks.size());
         assertTrue(filteredBooks.stream().anyMatch(book -> book.getTitle().equals("Title1")));
 
-        ///  The fourth case d1>d2 F&&F
+        //  The fourth case d1>d2 F&&F
         cal.set(2020, Calendar.JULY, 1);
         date1 = cal.getTime();
         cal.set(2018, Calendar.JULY, 3); 
